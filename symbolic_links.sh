2d95daa6bf183ac5653d2676237c5c37ce36acf4
veggietorta <ochoaje@gmail.com>
2019-02-13 15:24:29 -0800

    fucntions added to symbolic_links.sh

--- symbolic_links.sh
+++ symbolic_links.sh
@@ -1,12 +1,64 @@
 #! /bin/bash/
 
+#TODO test each function
+
+
 # vim
-	ln -s $HOME/dotfiles/vim/.vimrc $HOME/
+vim_link()
+    {
+        # check for existance of vimrc in $HOME directory
+    if [ -e ~/.vimrc ] || [ -L ~/.vimrc ]
+
+    then
+        # vimrc found, remove found vimrc and create symbolic link from vimrc in dotfiles
+        unlink ~/.vimrc # should a symbolic link already exist
+        rm ~/.vimrc &> /dev/null # &> /dev/null suppress error message.
+		echo a vimrc was found, removed and replaced
+
+    else
+        # vimrc not found, created symbolic link
+        ln -s ~/dotfiles/vim/.vimrc ~/
+		echo vimrc not found, created symbolic link
+    fi
+    }
 
 # bash
-	ln -s $HOME/dotfiles/bash/.bashrc $HOME/
-	ln -s $HOME/dotfiles/bash/.bash_aliases $HOME/
+bashrc_link()
+    {
+    if [ -e ~/.bashrc ]
+    then
+        rm ~/.bashrc
+	    ln -s ~/dotfiles/bash/.bashrc ~/
+    else
+        ln -s ~/dotfiles/bash/.bashrc ~/
+    fi
+    }
+
+bash_aliases_link()
+    {
+    if [ -e ~/.bash_aliases ]
+    then
+        rm ~/.bash_aliases
+        ln -s ~/dotfiles/bash/.bash_aliases ~/
+    else
+        ln -s ~/dotfiles/bash/.bash_aliases ~/
+    fi
+    }
 
 # tmux
-	ln -s $HOME/dotfiles/tmux/.tmux_config $HOME/
+tmux_link()
+    {
+    if [ -e ~/.tmux.conf ]
+    then
+        rm ~/.tmux.config
+        ln -s ~/dotfiles/tmux/.tmux.conf ~/
+    else
+        ln -s ~/dotfiles/tmux/.tmux.conf ~/
+    fi
+    }
+
+# vim_link
+# bashrc_link
+# bash_aliases_link
+# tmux_link
 
