5c201e474bc273a43b9aedd494c0d50c25b7d873
vt <vt@vt>
2019-10-04 07:09:23 -0700

    playing with settings

--- shared/qutebrowser/config.py
+++ shared/qutebrowser/config.py
@@ -6,19 +6,40 @@
 
 
 
-
 # Autoload autoconfig.yml
 config.load_autoconfig()
+
+# Start qutebrowser in private mode
+    #open -p
+
+
+
+# The directory to save downloads
+c.downloads.location.directory = "~/Downloads"
+
+# Leave insert mode if a non-editable element is clicked.
+c.input.insert_mode.auto_leave = True
+
+# Automatically enter insert mode if an editable element is focused after loading the page.
+c.input.insert_mode.auto_load = True
 
 
 # Search Engines
 {"DEFAULT": "https://duckduckgo.com/?q={}", "yt": "https://www.youtube.com/results?search_query={}"}
 
 
+# The page(s) to open at the start.
+c.url.start_pages = "https://duckduckgo.com/?={}/"
+
+
+
 # Keybinding:
-config.bind(';m', 'hint links userscript ~/.qutebrowser/script_yt')
+    #config.bind(';m', 'hint links userscript ~/.qutebrowser/script_yt')
+config.bind("Cmd+q", "wq")
 
 
+# CSS Themes
+#set content.user_stylesheets draculized-reddit.com.css
 
 # Dracula Theme:
 
