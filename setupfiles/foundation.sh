eea153a678f95e4eb8706bd77683ff3bf06c8e61
VeggieTorta <30427052+VeggieTorta@users.noreply.github.com>
2021-02-10 12:00:06 -0800

    Setup Foundation file
    outline git and curl install

---
+++ setupfiles/foundation.sh
@@ -0,0 +1,12 @@
+Author: @veggietorta
+Update: 02.10.21
+System: Ubuntu 20.10
+Purpose: Install programs and system files required to install other installs
+
+# Install curl
+sudo apt install curl -y
+
+# Install git
+sudo apt install git-all -y
+git config --global user.name "vt"
+git config --global user.email "vt@vt.com"
