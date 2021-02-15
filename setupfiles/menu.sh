ef5edd23fae001c173660d6df080bb85d2a00c56
vt <vt@vt>
2021-02-15 20:02:55 +0000

    updt

--- setupfiles/menu.sh
+++ setupfiles/menu.sh
@@ -2,7 +2,6 @@
 # Author: @veggietorta
 # Update: 02.14.21
 
-. $HOME/dotfiles/setupfiles/logging_script
 
 menu_ask_user()
 {
@@ -10,12 +9,12 @@ menu_ask_user()
     # Promp & read input
     # Load source based on input using If/then statement
 
-    echo $el_day_tim ": enter menu_ask_user" >> $el_log
+    echo $el_day_time ": Enter menu_ask_user" >> $el_log
     clear
-    echo "Select program to setup or modify:\n"
-    echo 1. zsh and Oh-my-zsh
-    echo 2. neovim
 
+    echo "Select program to setup or modify:\n"
+    echo "1. zsh and Oh-my-zsh"
+    echo "2. neovim\n"
     echo "q. exit"
 
     read main_menu_input
@@ -23,18 +22,17 @@ menu_ask_user()
     # Source ZSH file
     if [ $main_menu_input = '1' ]
     then
+        echo $el_day_time ": Option 1, Zsh file selected" >> $el_log
         . $HOME/dotfiles/setupfiles/zsh
-        echo $el_day_time ": sourced zsh file" >> $el_log
         menu_zsh
 
 
     # Source Neovim file
     elif [ $main_menu_input = '2' ]
     then
+        echo $el_day_time ": Option 2, Neovim file selected" >> $el_log
         . $HOME/dotfiles/setupfiles/neovim
-        echo $el_day_time ": sourced neovim file" >> $el_log
         menu_neovim
-
 
     # Exit
     elif [ $main_menu_input = 'q' ]
@@ -49,5 +47,5 @@ menu_ask_user()
 
 }
 
-
+. $HOME/dotfiles/setupfiles/logging_script
 log_it_up
