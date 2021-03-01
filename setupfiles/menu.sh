e76b3c967d122d601686c24a4e20a2248ea63bee
vt <vt@vt>
2021-02-28 19:13:37 -0800

    updt

--- setupfiles/menu.sh
+++ setupfiles/menu.sh
@@ -23,7 +23,9 @@ menu_ask_user()
     echo "6. time"
     echo "7. python3"
      # 8. youtube-dl
-     # 9. ssh
+    echo 9. SSH
+    echo 10. Wifi
+    # echo 11. Plex
 
     echo ""
     echo "q. exit"
@@ -31,73 +33,85 @@ menu_ask_user()
     read main_menu_input
 
 
-    # Source ZSH file
-    if [ $main_menu_input = '6' ]
-    then
-        echo $el_day_time": Option 6, time file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/time
-        menu_time
+    # Source SSH file
+    if [ $main_menu_input = '9' ]
+        then
+            echo $el_day_time": Option 9, time file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/ssh
+            menu_ssh
+
+    # Source wifi file
+    elif [ $main_menu_input = '10' ]
+        then
+            echo $el_day_time": Option 10, time file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/wifi
+            menu_wifi
+
+    # Source time file
+    elif [ $main_menu_input = '6' ]
+        then
+            echo $el_day_time": Option 6, time file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/time
+            menu_time
 
     # Source ZSH file
     elif [ $main_menu_input = '1' ]
-    then
-        echo $el_day_time": Option 1, Zsh file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/zsh
-        menu_zsh
-
+        then
+            echo $el_day_time": Option 1, Zsh file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/zsh
+            menu_zsh
 
     # Source Vim or Neovim file
     elif [ $main_menu_input = '2' ]
-    then
-        echo $el_day_time ": Option 2, Vim or Neovim file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/neovim_vim
-        menu_neovim
-
+        then
+            echo $el_day_time ": Option 2, Vim or Neovim file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/neovim_vim
+            menu_neovim
 
     # Source gnome-tweak-tool file
     elif [ $main_menu_input = '3' ]
-    then
-        echo $el_day_input ": Option 3, gnome-tweak-tool file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/gnome-tweak-tool
-        menu_gtl
-
+        then
+            echo $el_day_input ": Option 3, gnome-tweak-tool file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/gnome-tweak-tool
+            menu_gtl
 
     # Source vifm file
     elif [ $main_menu_input = '4' ]
-    then
-        echo $el_day_time ": Option 4, vifm file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/vifm
-        menu_vifm
+        then
+            echo $el_day_time ": Option 4, vifm file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/vifm
+            menu_vifm
 
     # Source virtualbox file
     elif [ $main_menu_input = '5' ]
-    then
-        echo $el_day_time ": Option 5, virtualbox file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/virtualbox
-        menu_virtualbox
+        then
+            echo $el_day_time ": Option 5, virtualbox file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/virtualbox
+            menu_virtualbox
 
 
     # Source Python3 file
     elif [ $main_menu_input = '7' ]
-    then
-        echo $el_day_time ": Option 7, virtualbox file selected" >> $el_log
-        . $HOME/dotfiles/setupfiles/python3
-        menu_python
+        then
+            echo $el_day_time ": Option 7, virtualbox file selected" >> $el_log
+            . $HOME/dotfiles/setupfiles/python3
+            menu_python
 
     # Exit
     elif [ $main_menu_input = 'q' ]
-    then
-        echo $el_day_time ": Option q, exit selected" >> $el_log
-        exit 1
-
+        then
+            echo $el_day_time ": Option q, exit selected" >> $el_log
+            exit 1
 
     # Input not received, start over
     else
+        #log
         echo $el_day_time ": Input not understood sent back to menu_ask_user" >> $el_log
+
         menu_ask_user
     fi
-
 }
+
 
 logger()
 {
